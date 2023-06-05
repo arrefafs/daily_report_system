bbbebed37f2ecb27730dc118f616ba14770da073
rikua <rikua@TABLET-GIJTJ15F>
2023-06-05 18:55:28 +0900

    編集

--- src/main/java/utils/EncryptUtil.java
+++ src/main/java/utils/EncryptUtil.java
@@ -1,15 +1,16 @@
+
 package utils;
-
-
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import javax.xml.bind.DatatypeConverter;
+
+
+//ハッシュ化処理を行うクラス
 public class EncryptUtil {
 
-
-  //生のパスワード文字列とpepper文字列を連結した文字列をSHA-256関数でハッシュ化し、返却する
+    //生のパスワード文字列とpepper文字列を連結した文字列をSHA-256関数でハッシュ化し、返却する
     public static String getPasswordEncrypt(String plainPass, String pepper) {
         String ret = "";
 
