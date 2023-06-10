a28558643c3cf3637acba40a13260afb5e2cd9e3
rikua <rikua@TABLET-GIJTJ15F>
2023-06-10 22:26:44 +0900

    ログイン中

--- src/main/java/filters/LoginFilter.java
+++ src/main/java/filters/LoginFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -22,7 +21,7 @@ import constants.ForwardConst;
  * Servlet Filter implementation class LoginFilter
  */
 
-@WebFilter("/*")
+//@WebFilter("/*")
 public class LoginFilter implements Filter {
 
     /**
